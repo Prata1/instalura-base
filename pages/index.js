0ee8a845401176e86b751ffd771c8bf4e05e4666
Prata1 <guilhermepratacosta@outlook.com.br>
2021-02-14 20:44:16 -0300

    Fix footer aula 04

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,19 @@
-import styled from 'styled-components'
 import Menu from '../src/components/commons/Menu'
+import Footer from '../src/components/commons/Footer'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
 
 export default function Home() {
   return ( 
-    <div>
+    <div style={{
+      flex: '1',
+      display: 'flex',
+      flexWrap: 'wrap',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+    }}>
       <Menu/>
       My page
+      <Footer />
     </div>
   )
 }
